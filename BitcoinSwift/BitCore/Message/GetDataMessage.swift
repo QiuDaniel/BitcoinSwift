7ad70adb3fca5775036f7feb961316e4a6bd1dfa
QiuDaniel <qiudan-098@163.com>
2022-02-08 16:15:43 +0800

    GetDataMessage update

--- BitcoinSwift/BitCore/Message/GetDataMessage.swift
+++ BitcoinSwift/BitCore/Message/GetDataMessage.swift
@@ -7,45 +7,24 @@
 
 import Foundation
 
-public enum GetDataType: Int32 {
-    case error = 0
-    case transaction = 1
-    case block = 2
-    case filtered = 3
-    case compact = 4
-    case unknown
-}
-
-public typealias Inventory = (GetDataType, Data)
-
 public struct GetDataMessage {
+    
+    public static let command = "getdata"
+    
     public let datas: [Inventory]
     
 
-    
-    public func serialize() throws -> Data {
+    public func serialize() -> Data {
         var result = VarInt(datas.count).serialize()
-        result += try datas.flatMap{ (type, identifier) -> Data in
-            if type.rawValue < 1 && type.rawValue > 4 {
-                throw BitCoreError.valueError("wrong GetDataType")
-            }
-            var data = type.rawValue.littleEndian.data
-            data += identifier.reversed()
-            return data
-        }
+        result +=  datas.flatMap{ $0.serialize() }
         return result
     }
     
-    public static func parse(_ stream: ByteStream) throws -> GetDataMessage {
+    public static func parse(_ stream: ByteStream) -> GetDataMessage {
         let count = stream.read(VarInt.self).underlyingValue
         var datas = [Inventory]()
         for _ in 0..<count {
-            let type = stream.read(Int32.self)
-            if type < 1 && type > 4 {
-                throw BitCoreError.valueError("wrong GetDataType")
-            }
-            let identifier = Data(stream.read(Data.self, count: 32).reversed())
-            datas.append((GetDataType(rawValue: type)!, identifier))
+            datas.append(Inventory.parse(stream))
         }
         return GetDataMessage(datas: datas)
     }
--- BitcoinSwift/BitCoreTests/GetDataMessageTests.swift
+++ BitcoinSwift/BitCoreTests/GetDataMessageTests.swift
@@ -21,26 +21,19 @@ class GetDataMessageTests: XCTestCase {
 
     func testSerialize() throws {
         let msgHex = "020300000030eb2540c41025690160a1014c577061596e32e426b712c7ca00000000000000030000001049847939585b0652fba793661c361223446b6fc41089b8be00000000000000"
-        let inventory1 = (GetDataType.filtered, Data(hex: "00000000000000cac712b726e4326e596170574c01a16001692510c44025eb30"))
-        let inventory2 = (GetDataType.filtered, Data(hex: "00000000000000beb88910c46f6b442312361c6693a7fb52065b583979844910"))
+        let inventory1 = Inventory(type: .filtered, identifier:  Data(hex: "00000000000000cac712b726e4326e596170574c01a16001692510c44025eb30"))
+        let inventory2 = Inventory(type: .filtered, identifier: Data(hex: "00000000000000beb88910c46f6b442312361c6693a7fb52065b583979844910"))
         let getData = GetDataMessage(datas: [inventory1, inventory2])
-        XCTAssertEqual(try! getData.serialize(), Data(hex: msgHex))
+        XCTAssertEqual(getData.serialize(), Data(hex: msgHex))
     }
     
     func testParse() {
         let stream = ByteStream(Data(hex: "020300000030eb2540c41025690160a1014c577061596e32e426b712c7ca00000000000000030000001049847939585b0652fba793661c361223446b6fc41089b8be00000000000000"))
-        let getData = try! GetDataMessage.parse(stream)
-        XCTAssertEqual(getData.datas[0].0, GetDataType.filtered)
-        XCTAssertEqual(getData.datas[0].1, Data(hex: "00000000000000cac712b726e4326e596170574c01a16001692510c44025eb30"))
-        XCTAssertEqual(getData.datas[1].0, GetDataType.filtered)
-        XCTAssertEqual(getData.datas[1].1, Data(hex: "00000000000000beb88910c46f6b442312361c6693a7fb52065b583979844910"))
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let getData = GetDataMessage.parse(stream)
+        XCTAssertEqual(getData.datas[0].type, GetDataType.filtered)
+        XCTAssertEqual(getData.datas[0].identifier, Data(hex: "00000000000000cac712b726e4326e596170574c01a16001692510c44025eb30"))
+        XCTAssertEqual(getData.datas[1].type, GetDataType.filtered)
+        XCTAssertEqual(getData.datas[1].identifier, Data(hex: "00000000000000beb88910c46f6b442312361c6693a7fb52065b583979844910"))
     }
 
 }
